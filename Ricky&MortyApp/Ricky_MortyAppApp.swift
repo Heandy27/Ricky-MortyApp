5601457b8c985b5f4a87692697eb2c47042f7994
Heandy27 <andy_heredia2008@hotmail.com>
2024-12-23 14:37:02 -0500

    Initial Commit

---
+++ Ricky&MortyApp/Ricky_MortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ricky_MortyAppApp.swift
+//  Ricky&MortyApp
+//
+//  Created by Andy Heredia on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Ricky_MortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
